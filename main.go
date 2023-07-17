29bc33fcb4ca53c887315a50a039c12d61f61ced
ZenoHwanEth <hwan2221@hotmail.com>
2023-07-17 21:08:58 +0800

    adding other handle function from mux

--- main.go
+++ main.go
@@ -22,4 +22,8 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movie", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 }
