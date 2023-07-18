99918f825d1f7f3cf04eec2ebe37cba565c81f61
ZenoHwanEth <hwan2221@hotmail.com>
2023-07-18 21:27:36 +0800

    adding details in updateMovie

--- main.go
+++ main.go
@@ -62,7 +62,20 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateMovie(w http.ResponseWriter, r *http.Request) {
-
+	//set json content type
+	w.Header().Set("Content-Type", "application/json")
+	// params
+	params := mux.Vars(r)
+	// loop over the movies, range
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
 }
 
 func main() {
